501d1bdc53eaa5fdff08cc37470e09cbac8bf3c2
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-02-26 00:46:35 +0000

    Updating JS
    - Adding event listener for clicks on delete button to remove last character from input string

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class Calculator {
   }
 
   delete() {
-
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
   }
 
   appendNumber(number) {
@@ -94,5 +94,10 @@ equalsButton.addEventListener('click', button => {
 
 allClearButton.addEventListener('click', button => {
   calculator.clear();
+  calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', button => {
+  calculator.delete();
   calculator.updateDisplay();
 });
