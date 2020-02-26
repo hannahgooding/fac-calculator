cdc3338462121532bf59bbf49bc004412585430c
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-02-26 00:24:27 +0000

    Updating JS
    - Adding event listeners for clicks on operation buttons
    - Adding function for appending operation to output

--- script.js
+++ script.js
@@ -21,7 +21,13 @@ class Calculator {
   }
 
   chooseOperation(operation) {
-
+    if (this.currentOperand === '') return;
+    if (this.previousOperand !== '') {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = '';
   }
 
   compute() {
@@ -30,6 +36,7 @@ class Calculator {
 
   updateDisplay() {
     this.currentOperandTextElement.innerText = this.currentOperand;
+    this.previousOperandTextElement.innerText = this.previousOperand;
   }
 };
 
@@ -47,6 +54,13 @@ const calculator = new Calculator(previousOperandTextElement, currentOperandText
 numberButtons.forEach(button => {
   button.addEventListener('click', () => {
     calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  })
+});
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText);
     calculator.updateDisplay();
   })
 });
