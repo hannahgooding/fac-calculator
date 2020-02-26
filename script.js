0267d95acdcdd5f479d62a91229f035d008a2c7e
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-02-26 01:03:07 +0000

    Updating JS
    - Displaying number input and operation in output above current operand before equals button is clicked by user

--- script.js
+++ script.js
@@ -58,8 +58,14 @@ class Calculator {
 
   updateDisplay() {
     this.currentOperandTextElement.innerText = this.currentOperand;
-    this.previousOperandTextElement.innerText = this.previousOperand;
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText =
+        `${this.previousOperand} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = '';
+    }
   }
+
 };
 
 // initialise buttons as variables
