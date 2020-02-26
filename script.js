b3208894ca2a6f64d78284d5e9d4d9249c791f0b
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-02-26 12:50:45 +0000

    Updating JS
    - Adding keyboard inputs for numbers

--- script.js
+++ script.js
@@ -107,3 +107,49 @@ deleteButton.addEventListener('click', button => {
   calculator.delete();
   calculator.updateDisplay();
 });
+
+// keyboard inputs
+document.onkeydown = function(e){
+  switch(e.keyCode){
+    case 48:
+      calculator.appendNumber(0);
+      calculator.updateDisplay();
+      break;
+    case 49:
+      calculator.appendNumber(1);
+      calculator.updateDisplay();
+      break;
+    case 50:
+      calculator.appendNumber(2);
+      calculator.updateDisplay();
+      break;
+    case 51:
+      calculator.appendNumber(3);
+      calculator.updateDisplay();
+      break;
+    case 52:
+      calculator.appendNumber(4);
+      calculator.updateDisplay();
+      break;
+    case 53:
+      calculator.appendNumber(5);
+      calculator.updateDisplay();
+      break;
+    case 54:
+      calculator.appendNumber(6);
+      calculator.updateDisplay();
+      break;
+    case 55:
+      calculator.appendNumber(7);
+      calculator.updateDisplay();
+      break;
+    case 56:
+      calculator.appendNumber(8);
+      calculator.updateDisplay();
+      break;
+    case 57:
+      calculator.appendNumber(9);
+      calculator.updateDisplay();
+      break;
+  }
+};
