00a78a4f200d6680b00c23399a34edc42f0d94a2
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-02-26 00:40:20 +0000

    Updating JS
    - Adding event listener for clicks on equals button
    - Adding compute function to perform calculations on user input

--- script.js
+++ script.js
@@ -31,7 +31,29 @@ class Calculator {
   }
 
   compute() {
-
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case '+':
+        computation = prev + current;
+        break;
+      case '-':
+        computation = prev - current;
+        break;
+      case '×':
+        computation = prev * current;
+        break;
+      case '÷':
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = '';
   }
 
   updateDisplay() {
@@ -63,4 +85,9 @@ operationButtons.forEach(button => {
     calculator.chooseOperation(button.innerText);
     calculator.updateDisplay();
   })
+});
+
+equalsButton.addEventListener('click', button => {
+  calculator.compute();
+  calculator.updateDisplay();
 });
