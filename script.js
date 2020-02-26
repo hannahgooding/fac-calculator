55e603371cbdbdb17c227cb74cdfd7df2d297eaf
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-02-26 00:44:47 +0000

    Updating JS
    - Adding event listener for clicks on AC button to perform clear function

--- script.js
+++ script.js
@@ -91,3 +91,8 @@ equalsButton.addEventListener('click', button => {
   calculator.compute();
   calculator.updateDisplay();
 });
+
+allClearButton.addEventListener('click', button => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
